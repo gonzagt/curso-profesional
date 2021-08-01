361116c49320b10655c03ffa49e512e6cb17dd88
gonzagt <gonzalogermantello@gmail.com>
2021-08-01 16:26:01 -0300

    actualizado script.js, ahora sí funcionando al 100%

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,26 @@
 const video = document.querySelector('video');
 const button = document.querySelector('button');
-var playing = true;
 
 function MediaPlayer(config) {
     this.media = config.el
 }
 
 MediaPlayer.prototype.play = function() {
-    playing = true;
     this.media.play();
 }
 
 MediaPlayer.prototype.pause = function() {
-    playing = false;
     this.media.pause();
+}
+
+MediaPlayer.prototype.togglePlay = function() {
+    if (this.media.paused) {
+        this.play();
+    } else {
+        this.pause();
+    }
 }
 
 const player = new MediaPlayer({ el: video });
 
-button.onclick = () => {
-    if (playing) {
-        player.pause();
-    } else {
-        player.play();
-    }
-}
+button.onclick = () => player.togglePlay();
