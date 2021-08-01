2a2aa8142b76541cba48b685dd1a85e78a4d0f74
gonzagt <gonzalogermantello@gmail.com>
2021-08-01 18:35:34 -0300

    Corregida una línea con un comando incompleto/innecesario

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,9 @@ import AutoPlay from "./plugins/autoplay.js";
 const video = document.querySelector('video');
 const button = document.querySelector('#playButton');
 const muteButton = document.querySelector('#muteButton');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, /*plugins: [new AutoPlay()]*/ });
 
 button.onclick = () => player.togglePlay();
 muteButton.onclick = () => {
     player.toggleMute()
-    muteButton.classList['Mute']
 };
