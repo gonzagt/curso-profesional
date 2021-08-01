bf0a62dcd14e5937a197ec18be0f4bc812ce6eed
gonzagt <gonzalogermantello@gmail.com>
2021-08-01 16:17:54 -0300

    actualizando el archivo script.js, con el botón 70% funcional (para iniciar la reproducción del video, hay que hacerle doble click. De ahí en adelante funciona bien, hasta que termina el video y de nuevo doble click)

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,27 @@
 const video = document.querySelector('video');
 const button = document.querySelector('button');
+var playing = true;
 
 function MediaPlayer(config) {
     this.media = config.el
 }
 
 MediaPlayer.prototype.play = function() {
+    playing = true;
     this.media.play();
+}
+
+MediaPlayer.prototype.pause = function() {
+    playing = false;
+    this.media.pause();
 }
 
 const player = new MediaPlayer({ el: video });
 
-button.onclick = () => player.play();
+button.onclick = () => {
+    if (playing) {
+        player.pause();
+    } else {
+        player.play();
+    }
+}
