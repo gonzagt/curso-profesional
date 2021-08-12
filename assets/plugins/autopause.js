63cf5786bf24ee8025a96cdcb34022c7e3b0f7f7
gonzagt <gonzalogermantello@gmail.com>
2021-08-12 19:25:45 -0300

    Añadida la funcionalidad de 'HandleVisibilityChange' al plugin de AutoPause, está genial, cambiás de pestaña y se pone en pausa automáticamente. :)

--- assets/plugins/autopause.js
+++ assets/plugins/autopause.js
@@ -2,18 +2,31 @@ class AutoPause {
     constructor(){
         this.threshold = 0.3
         this.handlerIntersection = this.handlerIntersection.bind(this)
+        this.handleVC = this.handleVC.bind(this)
+
     }
     run(player){
         this.player = player;
         const observer = new IntersectionObserver(this.handlerIntersection, { threshold: this.threshold })
         observer.observe(this.player.media);
-        console.log(observer);
+        // console.log(observer);
+        document.addEventListener("visibilitychange", this.handleVC);
     }
     
     handlerIntersection(entries) {
         const entry = entries[0]
 
         const isVisible = entry.intersectionRatio >= this.threshold
+
+        if (isVisible) {
+            this.player.play();
+        } else {
+            this.player.pause();
+        }
+    }
+
+    handleVC(){
+        const isVisible = document.visibilityState === "visible";
 
         if (isVisible) {
             this.player.play();
